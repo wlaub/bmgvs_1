f8719c8a33b2f266a967b779de9c794663e57da5
wlaub <goldcray@gmail.com>
2025-11-17 15:30:56 -0700

    more cleanups

--- player.py
+++ player.py
@@ -61,15 +61,15 @@ class Leg:
             dt = self.app.engine_time-self.active_time
             t = dt*self.speed
 
-#            print(t, self.app.player.left_leg == self)
             if t >= 1:
                 self.active = False
                 t = 1
 
             self.foot_body.position = self.active_position+self.active_direction*t
 
-#            pygame.draw.circle(self.app.screen, (0,128,0), self.active_position, 2)
-#            pygame.draw.circle(self.app.screen, (128,0,128), self.active_position+self.active_direction, 2)
+            if self.debug_draw:
+                pygame.draw.circle(self.app.screen, (0,128,0), self.active_position, 2)
+                pygame.draw.circle(self.app.screen, (128,0,128), self.active_position+self.active_direction, 2)
 
 
     def draw(self):
@@ -118,6 +118,7 @@ class Leg:
 
 
 class Player(Entity):
+    debug_draw = False
     def __init__(self, app, pos):
         super().__init__()
         self.health = 3
@@ -130,6 +131,7 @@ class Player(Entity):
         self.w =w= 10
         self.h =h= 17
         self.hips = 1
+        self.leg = leg = 3
 
         self.shape = pm.Poly(self.body, [
             (-w/2, -h+w),
@@ -137,39 +139,12 @@ class Player(Entity):
             (w/2, -h),
             (w/2, -h+w),
             ])
-
-        self.sensor_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-        self.sensor_body.position = self.body.position
-        self.sensor_shape = pm.Poly(self.sensor_body, [
-            (-w/2-1, -h+w+1),
-            (-w/2-1, -h-1),
-            (w/2+1, -h-1),
-            (w/2+1, -h+w+1),
-            ])
-        self.sensor_shape.sensor=True
-        self.sensor_shape.collision_type = COLLTYPE_DEFAULT
-        self.app.space.add(self.sensor_body, self.sensor_shape)
-
-
-#        self.shape = shape = pm.Poly.create_box(body, (10,20))
-#        self.shape.mass = m
         self.shape.collision_type = COLLTYPE_DEFAULT
 
         self.feets = []
 
-        self.leg = leg = 3
-
         self.left_leg = Leg(self.app, self.body, pos, leg, (-self.hips,0), m)
         self.right_leg = Leg(self.app, self.body, pos, leg, (self.hips,0), m)
-
-        self.center_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.set_center_position()
-        self.app.space.add(self.center_body)
-
-        c = pymunk.DampedSpring(self.center_body, self.body, (0,0), (0,0), 0, m*1000,1000000)
-        self.app.space.add(c)
-
-
 
         self.legs = [self.left_leg, self.right_leg]
         self.active_leg_idx = 0
@@ -177,23 +152,28 @@ class Player(Entity):
         self.active_leg.activate(0,0)
         self.walking = False
 
+        #body control
+        self.center_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        self.set_center_position()
+        self.app.space.add(self.center_body)
+
+        c = pymunk.DampedSpring(self.center_body, self.body, (0,0), (0,0), 0, m*1000,1000000)
+        self.app.space.add(c)
+
         self.angle = 0
 
         self.guns = []
         self.guns.append(Sord(self.app, self, Vec2d(12,-5), 1 ))
-#        self.guns.append(FaceGun(self.app, 1))
 
     def set_center_position(self):
         left = self.left_leg.foot_body.position
         right = self.right_leg.foot_body.position
 
+        #the body center should go down when the feet are far apart
         dist = max(0,abs(left-right)-self.w)
         alpha = 1-min(1,dist/(self.leg*2))
 
-        t = 5
         self.center_body.position = Vec2d(
-#                ((left.x+right.x)/2+self.body.position.x)/2,
-#                min(self.body.position.y-self.h/2, left.y-t, right.y-t)
                 (left.x+right.x)/2, (left.y+right.y)/2-self.leg*alpha
                 )
 
@@ -263,43 +243,42 @@ class Player(Entity):
         self._basic_hit_spell(dmg)
 
     def update(self):
-        self.friction = 0
+        self.friction =-10
 
-        speed = abs(self.body.velocity)
-        if speed > 0:
-            new_angle = math.atan2(self.body.velocity.y, self.body.velocity.x)
-            self.angle = new_angle
-
-        self.friction -=10
-
+        #guns
         for gun in self.guns:
             gun.update()
 
+        #damage TODO move this elsewhere?
         for ball in self.app.tracker[Ball]:
             try:
                 hit = self.shape.shapes_collide(ball.shape)
                 self.get_hit(1)
             except: AssertionError
 
-
+        #controls
+        speed = abs(self.body.velocity)
         controller = self.app.controller
-        dx, dy = controller.get_left_stick()
 
         fast_walk = controller.get_right_trigger()
-
+        dx, dy = controller.get_left_stick()
         stick_active = (dx*dx+dy*dy) > 0.5
+        aim = Vec2d(dx,dy)
+
+        #TODO move this leg control stuff into the proper leg controller
         if stick_active:
+            #start the next stick-driven step
             if not self.active_leg.active:
+                #toggle the active leg TODO: remove this? it does nothing?
                 if self.walking:
                     if self.active_leg == self.left_leg:
                         self.active_leg = self.right_leg
                     else:
                         self.active_leg = self.left_leg
 
+                #select the new active leg
                 left = self.left_leg.foot_body.position
                 right = self.right_leg.foot_body.position
-
-                aim = Vec2d(dx,dy)
 
                 if left.dot(aim) < right.dot(aim):
                     self.active_leg = self.left_leg
@@ -308,61 +287,60 @@ class Player(Entity):
                     self.active_leg = self.right_leg
                     other_leg = self.left_leg
 
+                """
+                select the position on a radius around the other foot that
+                maximizes the motion of their center of mass in the direction
+                of the stick
+                """
                 pos = self.active_leg.foot_body.position
                 other_pos = other_leg.foot_body.position
-                x1, y1 = pos - other_pos
-                x2 = x1+dx
-                y2 = y1+dy
                 dr = abs(aim)
                 dr2 = dr*dr
-                D = x1*y2-x2*y1
-                r = 2*self.leg
-                dis = r*r*dr2-D*D
-                if True:
-                    """
-                    select the position on a radius around the other foot that
-                    maximizes the motion of their center of mass in the direction
-                    of the stick
-                    """
-                    x1,y1 = pos
-                    x0,y0 = other_pos
+                x1,y1 = pos
+                x0,y0 = other_pos
 
-                    if not fast_walk:
-                        R = self.leg*1
-                        self.active_leg.speed = 7
-                    else:
-                        R = self.leg*7
-                        self.active_leg.speed = 3
+                if not fast_walk:
+                    R = self.leg*1
+                    self.active_leg.speed = 7
+                else:
+                    R = self.leg*7
+                    self.active_leg.speed = 3
 
-                    c1 = dr2*x0
-                    c2 = abs(dx*R*dr)
-
-                    sgn = -1 if dy < 0 else 1
-
-                    cx1 = (c1-c2)/dr2
-                    cx2 = (c1+c2)/dr2
-                    cy1 = y0 + sgn*math.sqrt(R*R+2*x0*cx1 -x0*x0 - cx1*cx1)
-                    cy2 = y0 + sgn*math.sqrt(R*R+2*x0*cx2 -x0*x0 - cx2*cx2)
-
-#                    pygame.draw.circle(self.app.screen, (255,0,0), other_pos, R, 1)
-
-                    p0 = Vec2d(cx1, cy1)
-                    p1 = Vec2d(cx2, cy2)
+                c1 = dr2*x0
+                c2 = abs(dx*R*dr)
 
 
-                    if (p0-pos).dot(aim) > (p1-pos).dot(aim):
-                        self.active_leg.activate_target(p0)
-                    else:
-                        self.active_leg.activate_target(p1)
+                sgn = -1 if dy < 0 else 1
+
+                cx1 = (c1-c2)/dr2
+                cx2 = (c1+c2)/dr2
+                cy1 = y0 + sgn*math.sqrt(R*R+2*x0*cx1 -x0*x0 - cx1*cx1)
+                cy2 = y0 + sgn*math.sqrt(R*R+2*x0*cx2 -x0*x0 - cx2*cx2)
+
+                if self.debug_draw:
+                    pygame.draw.circle(self.app.screen, (255,0,0), other_pos, R, 1)
+
+                p0 = Vec2d(cx1, cy1)
+                p1 = Vec2d(cx2, cy2)
+
+                #TODO there has got to be a better way to pick the branch
+                #pick the one that goes the furthest in the aim direction?
+                if (p0-pos).dot(aim) > (p1-pos).dot(aim):
+                    self.active_leg.activate_target(p0)
+                else:
+                    self.active_leg.activate_target(p1)
 
 
                 self.walking = True
 
+        #update legs
         self.active_leg.update()
 
+        #stop walking after last step
         if self.walking and not self.active_leg.active and not stick_active:
             self.walking = False
 
+        #deactivate and shufle in place
         if not self.walking and not stick_active and not self.active_leg.active:
             if self.active_leg == self.left_leg:
                 self.right_leg.deactivate(self.left_leg)
@@ -371,13 +349,12 @@ class Player(Entity):
                 self.left_leg.deactivate(self.right_leg)
                 self.active_leg = self.left_leg
 
-
+        #finally update the center based on where the feets are
         self.set_center_position()
 
+        #apply friction if slow walk
         if not fast_walk:
             self.body.apply_force_at_local_point(self.friction*self.body.velocity*self.m)
-
-#        self.mouse_body.position += Vec2d(dx,dy)
 
 
 class Sord(Entity):
@@ -419,68 +396,6 @@ class Sord(Entity):
 
 
 
-
-
-class FaceGun:
-    def __init__(self, app, r):
-        self.app = app
-        self.last_hit = self.app.engine_time
-
-        self.body = pm.Body(body_type = pm.Body.KINEMATIC)
-        self.shape = pm.Poly(self.body, [
-            (-r/2, r),
-            (-r/2, r+r),
-            (r/2, r+r),
-            (r/2, r),
-            ])
-        self.shape.sensor=True
-        self.shape.collision_type = COLLTYPE_DEFAULT
-
-        self.hit_angle = 0
-
-    def add_to_space(self, space):
-        space.add(self.body, self.shape)
-
-    def draw(self):
-        if self.fire :
-            body = self.body
-            poly = self.shape
-            ps = [p.rotated(body.angle) + body.position for p in poly.get_vertices()]
-            ps.append(ps[0])
-#            ps = list(map(self.app.flipyv, ps))
-            color = (0,128,0)
-            pygame.draw.lines(self.app.screen, color, False, ps)
-            pygame.draw.polygon(self.app.screen, color, ps)
-
-    def update(self):
-        controller = self.app.controller
-        player = self.app.player
-
-        now = self.app.engine_time
-
-        dt = now-self.last_hit
-        if now-self.last_hit > 2 and not controller.get_right_trigger():
-            self.last_hit = now
-            dt = 0
-            self.hit_angle = player.angle
-
-        self.fire = False
-        self.body.position = player.body.position
-        if dt < 1:
-            t = math.sin(dt*math.pi)
-
-            player.friction -= 25
-
-            self.fire = True
-            self.body.angle = self.hit_angle-t*3.14
-
-            for ball in self.app.tracker[Ball]:
-                try:
-                    hit = self.shape.shapes_collide(ball.shape)
-                    self.app.remove_entity(ball)
-                except: pass
-        else:
-            self.body.angle = player.angle
 
 
 
