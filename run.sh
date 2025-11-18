0ef0cd93c00dfda1592480d168b4a96de2e56c37
wlaub <goldcray@gmail.com>
2025-11-17 17:04:22 -0700

    fix run script for real this time no really

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ source venv/bin/activate
 
 checksum=`git rev-parse --short HEAD`
 status=`git status --porcelain --untracked-files=no | wc -l`
-if [ -z $status ]; then
+
+if [ $status -eq 0 ]; then
     title=$checksum
 else
     title=$checksum"+"
