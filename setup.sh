1e8f2f1a8d8cbda1fb369d45ff28b5cb2dd43aaf
wlaub <goldcray@gmail.com>
2025-11-17 18:07:05 -0700

    added setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+venv=venv
+
+if [ -e $venv ]; then
+    echo "already set up"
+else
+    mkdir $venv
+    virtualenv $venv --python=3
+    source $venv/bin/activate
+    pip install pygame pymunk
+fi
